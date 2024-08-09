384137a2719d7d53ef8b11594e9eab47cd50cc4d
Aiswarya2316 <aiswarya_pinkey@gmail.com>
2024-08-09 11:27:27 +0530

    petcare

--- pet_care/pet/views.py
+++ pet_care/pet/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(req):
+    return render(req, "index.html")
+
+def about(req):
+    return render(req, "about.html")  
+
+def contact(req):
+    return render(req, "contact.html")      
+
+def blog(req):
+    return render(req, "blog.html")    
